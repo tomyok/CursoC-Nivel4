7032d4b7038f17e140c01464d2abcf4707172523
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2026-02-24 12:49:53 -0300

    Agregar un nuevo método y una nueva vista para la pantalla “Bienvenida”. Agregar la opción de menú para acceder a la página. Dejar un texto de bienvenida en la página en cuestión. Por ejemplo: “Llegaste, te damos la bienvenida”.

--- ejemplo-1/Controllers/HomeController.cs
+++ ejemplo-1/Controllers/HomeController.cs
@@ -16,6 +16,11 @@ namespace ejemplo_1.Controllers
             return View();
         }
 
+        public IActionResult Bienvenida()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
